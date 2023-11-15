20de4040db3764e453af5810008a0b4755548412
dawn_lch <chung4111@naver.com>
2023-11-15 20:43:10 +0900

    231114

--- app/src/main/java/com/example/sampleconstraintlayout2/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout2/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//xml 파일을 파라미터로 전달하면 레이아웃 파일이 액티비티 파일과 연결됨.
+        setContentView(R.layout.button);//xml 파일을 파라미터로 전달하면 레이아웃 파일이 액티비티 파일과 연결됨.
     }
 
     @Override
